5e0b08a7fcbc6949858ddbafb24c34701f5ba475
Alexander Christensen <alex_c007@hotmail.com>
2017-01-11 18:16:02 +0100

    hello world

---
+++ obf_1.c
@@ -0,0 +1,53 @@
+// "This looks curios, I wonder what it does!\n"
+//////abcdef//////ghijkl//////mnopqr//////|t|||
+#     define      c     char
+      c W[  ]=    {0x77       ,(int)
+			'o'         ,/*##d      ef""i
+//n#e       */    114         ,108,
+      0144        };
+            #           define      yx_1 0
+#     define      syt_93      (6-(yx_1   +1))
+      /**/# define      v     void  //   /
+            /*+   qwdoi       )*/c*
+in(v        (*smth2 	)(c*        ,...)
+      ,int        caps,       c*    v0){
+	        #     define      i     int
+for(i j=          yx_1  ;j<         caps;
+	  j     ++          ){    smth2
+	        ("%c"       ,v0         [j
+])                ;           }
+	  #           define      r     return
+            r""         ;}i         out
+(i    hej         ,v(*        smth  )
+      (c                *,          ...
+		    ),c*              v1,   c*
+v2                )           {
+	  #     define            sjv   2
+            hej=        hej         ;in
+(smth             ,           syt_93
+      ,           v1);  in          (
+	        smth        ,           sjv,
+", "              );in        (     smth
+	  ,(    yx_1        +     syt_93
+		    ),          v2          );/*
+smth+             5*/         in(
+	  smth  ,                 sjv   ,
+	        "!\n"       );r         yx_1;
+#                 define      m     main
+}c H        [6                ]=    {
+	 0x48         ,           /*
+			...*/       0x65        ,
+108         ,                 0X6C  ,/*
+	 */           0157}             ;
+            #     define      us3_5 (yx_1*2)
+i           m                 (v    ){
+     #      include           <stdio.h>
+            #           define      p printf
+c*   h=           (c*         )H;c
+	 *w                 =           (
+		    c     *           )W;i
+hej               =           out
+	 (5,          (                 v*
+		    )           p,    h,w   )
+;hej        =     hej               +5
+	 ;      r     us3_5       ;     }
